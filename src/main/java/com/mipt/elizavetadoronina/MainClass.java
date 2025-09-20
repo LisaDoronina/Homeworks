e19792a4a03485741770f86beebfb42a5177a824
Доронина Елизавета Андреевна <shuralevakanon@gmail.com>
2025-09-20 18:53:03 +0300

    Edit MainClass.java

--- src/main/java/com/mipt/elizavetadoronina/MainClass.java
+++ src/main/java/com/mipt/elizavetadoronina/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
     private int someInt;
     private String someStr;
     protected static double someDoub;
-    public final long someLong;
+    public final long someLong = 1L;
 
     public static void main(String[] args) {
         for (int i = 0; i < 15; i++) {
